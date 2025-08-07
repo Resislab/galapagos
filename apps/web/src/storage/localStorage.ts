82f57caad90af43d557db78282e7ee1f17047e18
antoinelaborde <lab.antoine@gmail.com>
2025-08-07 10:41:02 +0200

    no need for local storage

--- apps/web/src/storage/localStorage.ts
+++
@@ -1,7 +0,0 @@
-export function getObjectFromLocalStorage<T>(key: string): T | undefined {
-    const item: string | null = localStorage.getItem(key);
-    if (item) {
-        return JSON.parse(item) as T;
-    }
-    return undefined;
-}
