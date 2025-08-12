1171246fba0bde20ee1b36aebecf48bfca8f0e5d
antoinelaborde <lab.antoine@gmail.com>
2025-08-12 15:14:15 +0200

    add timeout to supabase client

--- apps/web/src/api/client.ts
+++ apps/web/src/api/client.ts
@@ -5,8 +5,23 @@ import {createClient} from "@supabase/supabase-js";
 const supabaseURL = import.meta.env.VITE_SUPABASE_URL
 const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
-console.log("supabaseURL", supabaseURL)
-export const supabaseClient = createClient(supabaseURL, supabaseKey)
+export const supabaseClient = createClient(supabaseURL, supabaseKey, {
+    auth: {
+        persistSession: true
+    },
+    global: {
+        fetch: (url, options) => {
+            const controller = new AbortController();
+            const timeoutId = setTimeout(() => controller.abort(), 5000);
+
+            return fetch(url, {
+                ...options,
+                signal: controller.signal
+            }).finally(() => clearTimeout(timeoutId));
+        }
+
+    }
+})
 
 export const apiClient = axios.create({
     baseURL: import.meta.env.VITE_BACKEND_URL,
